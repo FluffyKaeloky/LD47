c59ef44d10a768d383e4d3a62175f11ce26b384d
Fluffy_Kaeloky <pierrejulienbaron@live.com>
2020-10-03 12:00:00 +0200

    Movements and camera

---
+++ LD47/Assets/_Game/Scripts/PlayerCamera.cs
@@ -0,0 +1,47 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RewiredPlayer))]
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform horizontalPivot = null;
+    public Transform verticalPivot = null;
+
+    public float minVerticalAngle = 75.0f;
+    public float maxVerticalAngle = -75.0f;
+
+    public float sensitivity = 1.0f;
+
+    [FoldoutGroup("InputNames")]
+    public string horizontalCameraInputName = "LookHorizontal";
+    [FoldoutGroup("InputNames")]
+    public string verticalCameraInputName = "LookVertical";
+
+    private RewiredPlayer player = null;
+
+    private float horizontal, vertical;
+
+    private float angle = 0.0f;
+
+    private void Awake()
+    {
+        player = GetComponent<RewiredPlayer>();
+    }
+
+    private void Update()
+    {
+        horizontal = player.Player.GetAxis(horizontalCameraInputName);
+        vertical = player.Player.GetAxis(verticalCameraInputName);
+    }
+
+    private void FixedUpdate()
+    {
+        horizontalPivot.Rotate(Vector3.up, horizontal * sensitivity);
+
+        angle = Mathf.Clamp(angle - vertical * sensitivity, minVerticalAngle, maxVerticalAngle);
+
+        verticalPivot.localRotation = Quaternion.AngleAxis(angle, Vector3.right);
+    }
+}
---
+++ LD47/Assets/_Game/Scripts/PlayerMovements.cs
@@ -0,0 +1,63 @@
+﻿using Rewired;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(RewiredPlayer))]
+public class PlayerMovements : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float walkMultiplier = 0.5f;
+
+    [FoldoutGroup("InputNames")]
+    public string horizontalMovementInputName = "Horizontal";
+    [FoldoutGroup("InputNames")]
+    public string verticalMovementInputName = "Vertical";
+    [FoldoutGroup("InputNames")]
+    public string walkInputName = "Walk";
+
+    private CharacterController controller = null;
+
+    private RewiredPlayer player = null;
+
+    private float vertical, horizontal;
+    private bool walk;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        player = GetComponent<RewiredPlayer>();
+    }
+
+    private void Update()
+    {
+        vertical = player.Player.GetAxis(verticalMovementInputName);
+        horizontal = player.Player.GetAxis(horizontalMovementInputName);
+        walk = player.Player.GetButton(walkInputName);
+    }
+
+    private void FixedUpdate()
+    {
+        /*Vector3 inputs = new Vector3(horizontal, 0.0f, vertical);
+        Debug.Log("Inputs : " + inputs);
+
+        if (forward != null)
+        {
+            float angle = forward.rotation.eulerAngles.y;
+            Debug.Log("Angle : " + angle);
+            inputs = Quaternion.AngleAxis(angle, Vector3.up) * inputs;
+            Debug.Log("Transformed Inputs : " + inputs);
+        }*/
+
+        Vector3 inputs = new Vector3(horizontal, 0.0f, vertical);
+
+        inputs = transform.rotation * inputs;
+
+        Vector3 delta = new Vector3(moveSpeed * inputs.x, 0.0f, moveSpeed * inputs.z) + (Physics.gravity * Time.fixedDeltaTime);
+        delta.Scale(walk ? Vector3.one * walkMultiplier : Vector3.one);
+
+        controller.Move(delta);
+    }
+}
