8125727f8e9a3979aa7851820b46e3aeb274c283
Fluffy_Kaeloky <pierrejulienbaron@live.com>
2020-10-04 17:21:05 +0200

    Turret stuff

--- LD47/Assets/_Game/Scripts/AI/Actions/TurretAim.cs
+++ LD47/Assets/_Game/Scripts/AI/Actions/TurretAim.cs
@@ -11,9 +11,21 @@ public class TurretAim : Action
 
     public SharedVector3 targetOffset;
 
+    public SharedVector3 horizontalRotationOffset;
+    public SharedVector3 verticalRotationOffset;
+
     public bool idle = false;
 
-	public override TaskStatus OnUpdate()
+    private Quaternion horizontalPivotRotation = Quaternion.identity;
+    private Quaternion verticalPivotRotation = Quaternion.identity;
+
+    public override void OnStart()
+    {
+        horizontalPivotRotation = horizontalPivot.Value.transform.localRotation;
+        verticalPivotRotation = verticalPivot.Value.transform.localRotation;
+    }
+
+    public override TaskStatus OnUpdate()
 	{
         if (target.Value == null && idle == false)
             return TaskStatus.Failure;
@@ -34,7 +46,13 @@ public class TurretAim : Action
             verticalTarget = Quaternion.AngleAxis(targetEuler.x, -transform.right) * Quaternion.AngleAxis(targetEuler.z, transform.forward);
         }
 
-        horizontalPivot.Value.transform.localRotation = Quaternion.Slerp(horizontalPivot.Value.transform.localRotation, horizontalTarget, Time.fixedDeltaTime * 8.0f);
-        verticalPivot.Value.transform.localRotation = Quaternion.Slerp(verticalPivot.Value.transform.localRotation, verticalTarget, Time.fixedDeltaTime * 8.0f);
+        horizontalPivotRotation = Quaternion.Slerp(horizontalPivotRotation, horizontalTarget * Quaternion.Euler(horizontalRotationOffset.Value), Time.fixedDeltaTime * 8.0f);
+        verticalPivotRotation = Quaternion.Slerp(verticalPivotRotation, verticalTarget * Quaternion.Euler(verticalRotationOffset.Value), Time.fixedDeltaTime * 8.0f);
+    }
+
+    public override void OnLateUpdate()
+    {
+        horizontalPivot.Value.transform.localRotation = horizontalPivotRotation;
+        verticalPivot.Value.transform.localRotation = verticalPivotRotation;
     }
 }
