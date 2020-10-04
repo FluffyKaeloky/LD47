6659bea4b8c3dd1dd728e239909946dce2a31e74
Fluffy_Kaeloky <pierrejulienbaron@live.com>
2020-10-04 11:13:33 +0200

    Meshes import settings (Disabled rig and animations), wip on death and hit feedback

---
+++ LD47/Assets/_Game/Scripts/PlayerDeath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Damageable))]
+public class PlayerDeath : MonoBehaviour
+{
+    public UnityEvent onPlayerDeath = new UnityEvent();
+
+    public void Awake()
+    {
+        Damageable damageable = GetComponent<Damageable>();
+
+        damageable.onDeath.AddListener(() => onPlayerDeath?.Invoke());
+    }
+}
---
+++ LD47/Assets/_Game/Scripts/ScreenFade.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScreenFade : MonoBehaviour
+{
+    public bool playOnStart = true;
+
+    public Color color = Color.black;
+
+    public UnityEvent onFadeEnd = new UnityEvent();
+
+    private Tween tween = null;
+
+    public void Start()
+    {
+        if (playOnStart)
+            FadeScreen(true);
+    }
+
+    public void FadeScreen(bool state)
+    {
+
+    }
+}
