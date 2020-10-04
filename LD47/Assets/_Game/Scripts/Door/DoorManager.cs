9bad6a82816f49e280e83be97b719884b189061e
nguyen <vngu.pro@gmail.com>
2020-10-04 12:06:17 +0200

    door automatic fix

---
+++ LD47/Assets/_Game/Scripts/Door/DoorManager.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    [SerializeField] Transform openTransform;
+    [SerializeField] Transform closeTransform;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            Open();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            Close();
+    }
+
+    public void Open()
+    {
+        transform.DOMove(openTransform.position, 1.0f).SetEase(Ease.OutExpo);
+        //transform.parent.position = openTransform.position;
+    }
+
+    public void Close()
+    {
+        transform.DOMove(closeTransform.position, 1.0f).SetEase(Ease.OutExpo);
+        //transform.parent.position = closeTransform.position;
+    }
+}
