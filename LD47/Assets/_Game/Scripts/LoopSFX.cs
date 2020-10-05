79224400c5e1815e4429672fe10ad9d04e96564b
Fluffy_Kaeloky <pierrejulienbaron@live.com>
2020-10-05 21:04:42 +0200

    Loop screen fade

--- LD47/Assets/_Game/Scripts/LoopSFX.cs
+++ LD47/Assets/_Game/Scripts/LoopSFX.cs
@@ -4,18 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LoopManagerScript))]
+[RequireComponent(typeof(ScreenFade))]
 public class LoopSFX : MonoBehaviour
 {
     public AudioObject endLoopSFX = null;
     public AudioObject startLoopSFX = null;
 
     private LoopManagerScript loopManager = null;
+    private ScreenFade fader = null;
 
     private bool loopEndSFXFired = false;
 
     private void Awake()
     {
         loopManager = GetComponent<LoopManagerScript>();
+        fader = GetComponent<ScreenFade>();
 
         loopManager.onTimerChanged.AddListener(OnTimerChanged);
         loopManager.onLoopStart.AddListener(OnLoopEnd);
@@ -32,6 +35,7 @@ public class LoopSFX : MonoBehaviour
         if (loopManager.loopTimer <= (endLoopSFX.clips[0].length + 1.0f))
         {
             MultiAudioManager.PlayAudioObject(endLoopSFX, transform.position);
+            fader.FadeScreen(true, endLoopSFX.clips[0].length);
             loopEndSFXFired = true;
         }
     }
@@ -42,6 +46,7 @@ public class LoopSFX : MonoBehaviour
             return;
 
         MultiAudioManager.PlayAudioObject(startLoopSFX, transform.position);
+        fader.FadeScreen(false, 0.5f);
 
         loopEndSFXFired = false;
     }
