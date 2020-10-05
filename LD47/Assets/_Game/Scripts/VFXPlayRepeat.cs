3d2759f4d2ac19c08b047a137f9a09a94cb948a2
Fluffy_Kaeloky <pierrejulienbaron@live.com>
2020-10-05 20:54:45 +0200

    Broken door FX + stuff

---
+++ LD47/Assets/_Game/Scripts/VFXPlayRepeat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+public class VFXPlayRepeat : MonoBehaviour
+{
+    public float minDelay = 1.0f;
+    public float maxDelay = 2.0f;
+
+    private VisualEffect vfx = null;
+
+    private void Awake()
+    {
+        vfx = GetComponent<VisualEffect>();
+    }
+
+    private async void Start()
+    {
+        while (true)
+        {
+            vfx.Play();
+
+            await new WaitForSeconds(Random.Range(minDelay, maxDelay));
+        }
+    }
+}
