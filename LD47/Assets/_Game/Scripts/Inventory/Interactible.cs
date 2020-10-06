c2adf16fdce8fa4847fea7b434986dbcb5d0aa05
nguyen <vngu.pro@gmail.com>
2020-10-06 19:39:50 +0200

    door fix

--- LD47/Assets/_Game/Scripts/Inventory/Interactible.cs
+++ LD47/Assets/_Game/Scripts/Inventory/Interactible.cs
@@ -11,6 +11,8 @@ public class Interactible : MonoBehaviour
     public ItemObject itemNeed;
     InteractibleManager interactibleManager;
     [SerializeField] public TMP_Text TextDisplay;
+    [SerializeField] GameObject redLock;
+    [SerializeField] GameObject greenUnlock;
     private void Awake()
     {
         interactibleManager = GetComponent<InteractibleManager>();
@@ -19,7 +21,13 @@ public class Interactible : MonoBehaviour
     {
         if (other.tag == "Player")
             if (itemNeed == other.GetComponent<PlayerInventory>().inventory.GotItem(itemNeed))
-                interactibleManager.Open(); 
+            {
+                interactibleManager.Open();
+                redLock.SetActive(false);
+                greenUnlock.SetActive(true);
+                TextDisplay.text = "Open";
+            }
+               
     }
 
     private void OnTriggerExit(Collider other)
